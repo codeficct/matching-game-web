dd221c4bbb8da378b6f683e3c9921d9b83e1c884
Luis Gabriel Janco <janco7249@gmail.com>
2023-01-25 16:38:18 -0400

    feat: add navigation component to renderer links

---
+++ components/Navigation.jsx
@@ -0,0 +1,29 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import { links } from '../utils/links'
+import styles from './navigation.module.css'
+
+export default function Navigation () {
+  return (
+    <header className={styles.header}>
+      <nav className={styles.navigation}>
+        <Link href='/' className={styles.title}>
+          <h1>
+            Matching Game
+          </h1>
+          <Image src='/logo.png' priority width={26} height={26} alt='Matching Game ficct' />
+        </Link>
+        <ul>
+          {links.map(({ route, label, icon }) => (
+            <li key={route}>
+              <Link href={route}>
+                {icon}
+                <span>{label}</span>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  )
+}
---
+++ utils/links.js
@@ -0,0 +1,5 @@
+import { MdOutlineLeaderboard } from 'react-icons/md'
+
+export const links = [
+  { label: 'Clasificación', route: '/leaderboard', icon: <MdOutlineLeaderboard size={24} /> }
+]
