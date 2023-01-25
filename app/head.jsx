6db287676da714ab6b59d19b99535b0e13f61d92
Luis Gabriel Janco <janco7249@gmail.com>
2023-01-25 16:37:42 -0400

    feat: add seo on head

---
+++ app/head.jsx
@@ -0,0 +1,10 @@
+export default function Head () {
+  return (
+    <>
+      <title>Matching Game</title>
+      <meta content='width=device-width, initial-scale=1' name='viewport' />
+      <meta name='description' content='Matching Game, El juego de memoria visual de combinaciones. Proyecto de investigación para la Feria de Proyectos de Investigación 2-2022 de la FICCT-UAGRM' />
+      <link rel='icon' href='/favicon.ico' />
+    </>
+  )
+}
