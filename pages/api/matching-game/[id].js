38355e7dc28eb33fc2e91021519cf9fa11df1670
Luis Gabriel Janco <janco7249@gmail.com>
2023-01-28 09:50:04 -0400

    feat: add dynamic endpoint to update score

---
+++ pages/api/matching-game/[id].js
@@ -0,0 +1,20 @@
+import { dbConnect } from 'db'
+import User from 'db/models/User'
+
+dbConnect()
+
+// eslint-disable-next-line space-before-function-paren
+export default async function handlerUserId(req, res) {
+  const { method, query: { id }, body } = req
+
+  if (method !== 'PUT') {
+    return res.status(405).json({ message: 'Método no permitido.' })
+  }
+  try {
+    const user = await User.findByIdAndUpdate(id, body, { new: true, runValidators: true })
+    if (!user) return res.status(400).json({ message: 'Usuario no encontrado.' })
+    return res.status(200).json(user)
+  } catch (error) {
+    return res.status(400).json({ message: 'Error al actualizar el usuario.', error: error.message })
+  }
+}
