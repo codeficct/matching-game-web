b7dfd45b19225efd12003ca0e4500b5a7a91a0ca
Luis Gabriel Janco <janco7249@gmail.com>
2023-01-28 09:55:24 -0400

    feat: add root endpoint

--- pages/api/matching-game.js
+++ pages/api/matching-game.js
@@ -1,14 +1,20 @@
+import User from 'db/models/User'
+import { dbConnect } from '../../db'
+dbConnect()
 // eslint-disable-next-line space-before-function-paren
 export default async function handler(req, res) {
-  const { name } = req.query
   const { method } = req
   switch (method) {
-    case 'GET':
-      console.log(method)
-      return res.status(200).json(`Hello ${name}!`)
+    case 'GET': {
+      const users = await User.find({})
+      return res.status(200).json(users)
+    }
     case 'POST':
     case 'PUT':
-    case 'DELETE':
+    case 'DELETE': {
+      await User.deleteMany({})
+      return res.status(204).json({ message: 'Users deleted' })
+    }
     default:
       return res.status(405).end(`Method ${method} Not Allowed`)
   }
