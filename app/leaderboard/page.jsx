3898013622699afb503085d0933accc4c7393e18
Luis Gabriel Janco <janco7249@gmail.com>
2023-02-05 19:34:45 -0400

    feat: fetch users to display score

--- app/leaderboard/page.jsx
+++ app/leaderboard/page.jsx
@@ -1,5 +1,8 @@
+import Image from 'next/image'
+import styles from './leaderboard.module.css'
+
 function fetchPost () {
-  return fetch('https://jsonplaceholder.typicode.com/posts', {
+  return fetch('http://localhost:3000/api/matching-game', {
     next: {
       revalidate: 10
     }
@@ -7,17 +10,27 @@ function fetchPost () {
     .then(response => response.json())
 }
 export default async function LeaderboardPage () {
-  const posts = await fetchPost()
+  const users = await fetchPost()
   return (
-    <>
-      <h2>Clasificación - page</h2>
-      <section style={{ padding: '0 1rem' }}>
-        {posts.map(({ id, title, body }) => (
-          <article key={id}>
-            <h3 style={{ color: '#0099ff' }}>{title}</h3>
-            <p>{body}</p>
-          </article>))}
+    <main className={styles.leaderboard}>
+      <section className={styles.users}>
+        <header className={styles.header}>
+          <span>Usuario</span>
+          <span>Puntos</span>
+          <span title='Posición'>Pos</span>
+        </header>
+        <ul className={styles.userContainer}>
+          {users.map(({ _id, name, photo, score }, index) => (
+            <li className={styles.userItem} key={_id}>
+              <figure className={styles.photo}>
+                <Image src={photo} alt={name} fill sizes='100vw' priority />
+              </figure>
+              <h3>{name}</h3>
+              <span>{score}</span>
+              <div>{index + 1}</div>
+            </li>))}
+        </ul>
       </section>
-    </>
+    </main>
   )
 }
