136e0aea0d0d3ec006f0aa48ce24019f34b2d32e
Luis Gabriel Janco <janco7249@gmail.com>
2023-02-04 22:40:55 -0400

    refactor: move demo and hero in new components

--- app/page.jsx
+++ app/page.jsx
@@ -1,32 +1,32 @@
+import Demo from '@/demo/Demo'
+import Hero from '@/Hero'
 import Image from 'next/image'
 import styles from './home.module.css'
 
 export default function HomePage () {
   return (
     <main className={styles.main}>
-      <section className={styles.hero}>
-        <figure className={styles.image}>
+      <Demo />
+      <Hero />
+      <h3 className={styles.firstSubtitle}>Desarrolla tus habilidades cognitivas como:</h3>
+      <section className={styles.firstContent}>
+        <div>
+          <span>📦 Potenciar la memoria visual</span>
+          <span>🧠 Evitar la memoria a corto plazo</span>
+          <span>👨🏻‍💻 El reconocimiento de patrones que es un plus para un desarrollador de software.</span>
+        </div>
+        <figure className={styles.imageApp}>
           <Image
-            src='/matching-game.png' alt='Matching Game'
+            src='/demo-3.png' alt='Matching Game - app'
             fill
             sizes='100vw'
             priority
           />
+          {/* <img className={styles.shadow} src='/demo-shadow.png' alt='matching game' /> */}
         </figure>
-        <div className={styles.content}>
-          <h2 className={styles.title}>
-            EL JUEGO DE <span className={styles.blue}>MEMORIA VISUAL</span> DE <span className={styles.yellow}>COMBINACIONES</span>
-          </h2>
-          <p className={styles.leyend}>El emparejamiento es una habilidad esencial que nos ayuda a mejorar una serie de habilidades cognitivas</p>
-        </div>
       </section>
-      <section className={styles.firstContent}>
-        <p>Desarrolla tus habilidades cognitivas como:
-        </p>
-        <span>📦 La memoria visual</span>
-        <span>🧠 Evitar la memoria a corto plazo</span>
-        <span>👨🏻‍💻 El reconocimiento de patrones que es un plus para un desarrollador de software.</span>
-        {/* <p>Emparejar un objeto con otro es una tarea compleja, y se vuelve especialmente complicado cuando haces coincidir algo más abstracto, como una imagen, un color o un sonido. La combinación también ayuda con el enfoque.</p> */}
+      <section className={styles.secondContent}>
+        <p>Emparejar un objeto con otro es una tarea compleja, y se vuelve especialmente complicado cuando haces coincidir algo más abstracto, como una imagen, un color o un sonido. La combinación también ayuda con el enfoque.</p>
       </section>
     </main>
   )
