79ecfd4d3af01503de7776ae6301dd958114c881
Luis Gabriel Janco <janco7249@gmail.com>
2023-02-05 20:09:36 -0400

    feat: filter users and order with the highest score

--- pages/api/matching-game/index.js
+++ pages/api/matching-game/index.js
@@ -1,12 +1,12 @@
 import User from 'db/models/User'
-import { dbConnect } from '../../db'
+import { dbConnect } from '../../../db'
 dbConnect()
 // eslint-disable-next-line space-before-function-paren
 export default async function handler(req, res) {
   const { method } = req
   switch (method) {
     case 'GET': {
-      const users = await User.find({})
+      const users = await User.find({}).select('name email score photo').sort({ score: -1 })
       return res.status(200).json(users)
     }
     case 'POST':
