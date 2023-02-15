37a21d139d6dc7a7374303ab84fcae83c9c6d43a
Luis Gabriel Janco <janco7249@gmail.com>
2023-02-15 16:05:48 -0400

    refactor: add maxLevel filter when get user

--- pages/api/matching-game/index.js
+++ pages/api/matching-game/index.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
   const { method } = req
   switch (method) {
     case 'GET': {
-      const users = await User.find({}).select('name email score photo').sort({ score: -1 })
+      const users = await User.find({}).select('name email score photo maxLevel').sort({ score: -1 })
       return res.status(200).json(users)
     }
     case 'POST':
