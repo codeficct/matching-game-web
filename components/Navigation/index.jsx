298b743f3f3117daa4e9d7eabcdb930c3242d52a
Luis Gabriel Janco <janco7249@gmail.com>
2023-02-21 10:02:24 -0400

    fix: move navigation component in new file order

---
+++ components/Navigation/index.jsx
@@ -0,0 +1,27 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import { links } from '../../utils/links'
+import styles from './navigation.module.css'
+
+export default function Navigation () {
+  return (
+    <header className={styles.header}>
+      <nav className={styles.navigation}>
+        <Link href='/' className={styles.title}>
+          <h1>Matching Game</h1>
+          <Image src='/mgicon.png' priority width={36} height={36} alt='Matching Game ficct' />
+        </Link>
+        <ul>
+          {links.map(({ route, label, icon }) => (
+            <li key={route}>
+              <Link href={route}>
+                {icon}
+                <span>{label}</span>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  )
+}
